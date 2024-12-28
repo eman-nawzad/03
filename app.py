1fea9371f07aa1f0786e8a08703719bce877b208
eman-nawzad <eman.nawzad00@gmail.com>
2024-12-28 13:49:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import streamlit as st
-import geopandas as gpd
+import json
 import folium
+import streamlit as st
 from streamlit_folium import st_folium
 
 # Title
@@ -8,7 +8,8 @@ st.title("Drought Monitoring Application")
 
 # Load GeoJSON data
 geojson_path = "SVI_NDVI_Export.geojson"
-data = gpd.read_file(geojson_path)
+with open(geojson_path, "r") as f:
+    data = json.load(f)
 
 # Create a Folium map
 m = folium.Map(location=[35.5, 42.85], zoom_start=8)
@@ -19,9 +20,10 @@ st_folium(m, width=700, height=500)
 
 # Display information about the data
 st.sidebar.title("Drought Data Details")
-for feature in data.iterfeatures():
+for feature in data["features"]:
     properties = feature["properties"]
     st.sidebar.write(f"Region: {properties.get('name', 'N/A')}")
     st.sidebar.write(f"SPI: {properties.get('SPI', 'N/A')}")
     st.sidebar.write(f"NDVI: {properties.get('NDVI', 'N/A')}")
     st.sidebar.write("---")
+
